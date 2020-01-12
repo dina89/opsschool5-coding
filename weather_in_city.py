03c83e2367e4a9b0890274d439005ad9d52d8db9
dina89 <dstefansky@gmail.com>
2020-01-12 22:20:20 +0200

    Submit homework3

--- weather_in_city.py
+++ weather_in_city.py
@@ -1,5 +1,7 @@
 from requests import get
-import sys
+import click
+
+
 
 def is_json_key_present(json, key):
     try:
@@ -8,33 +10,31 @@ def is_json_key_present(json, key):
         return False
     return True
 
-def get_weather(key, city, unit):
-    try:
-        api_url = "http://api.weatherstack.com/current?access_key={}&query={}&units={}".format(key, city, unit)
+@click.command()
+@click.option('--token', 'token', help='Token for my weather site')
+@click.option('--city', 'city', help='List of cities')
+@click.option('--T', 'temp', help='Celsius or Fahrenheit')
+def get_weather(token, city, temp):
+    cities = city.split(",")
+    if temp == "Fahrenheit":
+        unit = "f"
+    else:
+        unit = "m"
+    for city in cities:
+        api_url = "http://api.weatherstack.com/current?access_key={}&query={}&units={}".format(token, city, unit)
         result = get(api_url)
         json_result = result.json()
         if is_json_key_present(json_result, "success"):
-            raise ValueError("No data for city")
+            if json_result["error"]["type"] == "invalid_access_key":
+                print("Please provide a valid key.")
+                exit()
+            else:
+                print("Please choose an existing city like New York, Antwerp and Kathmandu.")
+                exit()
         temperature = json_result["current"]["temperature"]
-        if unit == "f":
-            unit = "Fahrenheit"
-        else:
-            unit = "Celsius"
-        print("The weather in {} today is {} {}.".format(city, temperature, unit))
-    except ValueError:
-        print("Please choose an existing city like New York, Antwerp and Kathmandu.")
+        print("The weather in {} today is {} {}.".format(city, temperature, temp))
 
 
 
 if __name__ == '__main__':
-    key = sys.argv[1]
-    cities = sys.argv[2].split(",")
-    if sys.argv[3] == "-f":
-        unit = "f"
-    elif sys.argv[3] == "-c":
-        unit = "m"
-    else:
-        print("Please choose -f or -c for type of degrees")
-        exit()
-    for city in cities:
-        get_weather(key, city, unit)
+    get_weather()
